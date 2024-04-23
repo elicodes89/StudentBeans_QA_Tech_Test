1649cfbb6292cfe418fe495beb29db5b02baf016
elisol bastouri <elicodes89@gmail.com>
2024-04-23 12:39:47 +0100

    Fix spelling mistake

--- test/pageObjects/parentPageObject.js
+++ test/pageObjects/parentPageObject.js
@@ -1,9 +1,14 @@
-const { assert } = require('chai')
+const { expect , assert } = require('chai')
 
 class ParentPageObject {
   async isElementEqualToExpected (element, expectedText) {
-    const rrorMessage = 'Actual does not match expected'
+    const errorMessage = 'Actual does not match expected'
     assert(await expect(await element.getText(), errorMessage).to.equal(expectedText))
+  }
+
+  async isElementDisplayed (element){
+    const errorMessage = 'Element is not displayed'
+    assert(await expect(await element, errorMessage).be.true)
   }
 }
 
